ec13ea76abb4806da1cc623d20818fc705d99528
Tolui <tobi418@gmail.com>
2021-04-18 16:06:41 +0800

    quest added

--- src/pages/dex.mn/TradePage.tsx
+++ src/pages/dex.mn/TradePage.tsx
@@ -39,7 +39,9 @@ const Wrapper = styled.div`
 `;
 
 export default function TradePage() {
-  const { marketAddress } = useParams();
+  const marketAddress = setQuest();
+  //const { marketAddress } = useParams();
+  console.log(marketAddress)
   useEffect(() => {
     if (marketAddress) {
       localStorage.setItem('marketAddress', JSON.stringify(marketAddress));
@@ -58,6 +60,28 @@ export default function TradePage() {
       <TradePageInner />
     </MarketProvider>
   );
+}
+
+function setQuest(){
+  const questAddress = "HTQDqzSsXM7bv5q3V4n1EGRBt1tziJWUx7ATfx1ETYKb";
+  const questMarket = {
+    address: questAddress,
+    baseLabel: "QUEST",
+    name: "QUEST/USDT",
+    programId: "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin"
+  }
+  const existingCustomMarkets = localStorage.getItem("customMarkets");
+  let newCustomMarkets:any[] = [];
+  if(existingCustomMarkets){
+    newCustomMarkets = JSON.parse(existingCustomMarkets);
+  }
+  newCustomMarkets.push(questMarket);
+  newCustomMarkets = Array.from(new Set(newCustomMarkets.map(a => a.address)))
+  .map(address => {
+    return newCustomMarkets.find(a => a.address === address)
+  });
+  localStorage.setItem("customMarkets", JSON.stringify(newCustomMarkets));
+  return questAddress;
 }
 
 function TradePageInner() {
