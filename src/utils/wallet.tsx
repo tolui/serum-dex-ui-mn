55f1b13c19140402a6e1fc11d5b71efac8d72a9b
Tolui <tobi418@gmail.com>
2021-04-19 22:41:56 +0800

    wallet added

--- src/utils/wallet.tsx
+++ src/utils/wallet.tsx
@@ -24,6 +24,11 @@ const ASSET_URL =
   'https://cdn.jsdelivr.net/gh/solana-labs/oyster@main/assets/wallets';
 export const WALLET_PROVIDERS = [
   {
+    name: 'wallet.dex.mn',
+    url: 'https://wallet.dex.mn',
+    icon: `${ASSET_URL}/sollet.svg`,
+  },
+  {
     name: 'sollet.io',
     url: 'https://www.sollet.io',
     icon: `${ASSET_URL}/sollet.svg`,
